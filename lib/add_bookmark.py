c2a9daef92a9facae79e6f377917fefc55da0c4e
rgrannell1 <r.grannell2@gmail.com>
2015-01-18 15:18:31 +0000

    working on inserts.

---
+++ lib/add_bookmark.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import html
+import sql
+import utils
+
+from result import Success, Failure
+
+
+
+
+def add_bookmark(db_result, path):
+
+	insert_result = (
+		Success(path)
+		.then( lambda path: sql.insert_bookmark(db_result, 'hi', path, utils.now()) )
+	)
+
+	return path
