c3e67567cdb71f558d51d3efbbda2a9768c5c3dd
rgrannell1 <r.grannell2@gmail.com>
2015-01-18 10:38:10 +0000

    working on search filters

--- lib/engram.py
+++ lib/engram.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-from result import Success, Failure
-from flask import Flask, redirect, url_for
+from result           import Success, Failure
+from flask            import Flask, redirect, url_for, request
 
-from database import Database
+from database         import Database
 
 import sql
 import html
-from show_bookmarks import show_bookmarks
+from show_bookmarks   import show_bookmarks
+from criteria         import criteria
 
 from extract_metadata import extract_metadata
 
@@ -45,14 +46,24 @@ def main():
 
 
 
-	@app.route("/bookmarks/<int:id>", methods = ["DELETE"])
-	def delete_page(id):
-		return "deleting %d" % id
-
 
 	@app.route("/bookmarks/search")
 	def search_routes():
-		return show_bookmarks({'sort': 10}, db)
+
+		print (
+			Success(None)
+			.then(lambda _: request)
+		)
+
+		print(criteria(request))
+
+		return "running."
+
+
+	@app.route("/bookmarks/<int:id>", methods = ["DELETE"])
+	def delete_page(id):
+		1
+
 
 
 
