cf7dcbca2aef4411d80f0adc51657f5ab4a2351a
rgrannell1 <r.grannell2@gmail.com>
2015-01-19 12:36:41 +0000

    added early removal to give the appearence of a quick DB call.

--- public/javascript/delete-bookmark.js
+++ public/javascript/delete-bookmark.js
@@ -5,14 +5,21 @@ $('.delete-bookmark').click(function () {
 	const $article = $(this).closest('article')
 	const id       = $(this).closest('article').attr('id')
 
+	$article.hide()
+
 	$.ajax({
 		url: '/bookmarks/' + id,
 		type: 'DELETE',
 		success: function () {
+
 			$article.remove()
+
 		},
 		failure: function () {
-			alert('failed.')
+
+			alert('oh dear. delete failed.')
+			$article.show()
+
 		}
 	})
 
