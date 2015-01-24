065e8f703412f758f257f0d480cdf6a139d6cb64
rgrannell1 <r.grannell2@gmail.com>
2015-01-24 19:58:43 +0000

    made selector more efficient

--- public/javascript/jquery-viewport.js
+++ public/javascript/jquery-viewport.js
@@ -1,29 +1,15 @@
-/*
-
-    $.belowthefold = function(element, settings) {
-        var fold = $(window).height() + $(window).scrollTop();
-        return fold <= $(element).offset().top - settings.threshold;
-    };
-    $.abovethetop = function(element, settings) {
-        var top = $(window).scrollTop();
-        return top >= $(element).offset().top + $(element).height() - settings.threshold;
-    };
-*/
-
-
-
-
 
 const inView = function ($elems) {
 
 	var $window = $(window)
+	var top     = $window.scrollTop()
 
 	return $elems.filter(function (ith) {
 
 		var $this   = $(this)
 		var offset  = $this.offset()
 
-		return top < offset.top + $this.height() && ($window.height() + $window.scrollTop()) > offset.top
+		return top < offset.top + $this.height() && ($window.height() + top) > offset.top
 
 	})
 
