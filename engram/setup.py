1750f9dfd13eecded85eccf07f99912957a5bbbd
rgrannell1 <r.grannell2@gmail.com>
2015-01-31 18:48:10 +0000

    added setup

---
+++ engram/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+
+from distutils.core import setup
+
+
+
+
+setup(
+	name             = 'Engram',
+	version          = '0.1.0',
+	description      = 'Minimal Bookmarking.',
+	author           = 'Ryan Grannell',
+	url              = 'https://github.com/rgrannell1/engram.py',
+	packages         = ['engram'],
+	install_requires = []
+)
