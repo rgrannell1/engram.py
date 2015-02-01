095bdb1f7efc548fb23a11a1960f63085df4c9f7
rgrannell1 <r.grannell2@gmail.com>
2015-02-01 04:11:37 +0000

    updated search alignment

--- public/javascript/lcs.js
+++ public/javascript/lcs.js
@@ -15,57 +15,42 @@ const charScore = function (char0, char1) {
 charScore.matchScore     = +2
 charScore.caseMatchScore = +1
 charScore.mismatchScore  = -Infinity
+charScore.gap            = -1
 
 
 
 
 
+const searchAlign = function (str0, str1) {
 
+	var cache = []
 
+	for (var ith = 0; ith <= str0.length; ++ith) {
 
+		var row = []
 
-
-const searchAlign = ( function () {
-
-	var align = function (score, str0, str1) {
-
-		var cache = []
-
-		for (var ith = 0; ith <= str0.length; ++ith) {
-
-			var row = []
-
-			for (var jth = 0; jth <= str1.length; ++jth) {
-				row[jth] = ith * jth === 0 ? (ith * -1) + (jth * -1): undefined
-			}
-
-			cache[ith] = row
-
+		for (var jth = 0; jth <= str1.length; ++jth) {
+			row[jth] = ith * jth === 0 ? (ith * -1) + (jth * -1): undefined
 		}
 
-		for (var ith = 1; ith <= str0.length; ++ith) {
-			for (var jth = 1; jth <= str1.length; ++jth) {
+		cache[ith] = row
 
-				cache[ith][jth] = Math.max(
-					0,
-
-					cache[ith - 1][jth - 1] + score.charPair(str0.charAt(ith + 1), str1.charAt(jth + 1)),
-					cache[ith    ][jth - 1] + score.gap,
-					cache[ith - 1][jth    ] + score.gap
-				)
-
-			}
-		}
-
-		return cache[str0.length][str1.length]
 	}
 
+	for (var ith = 1; ith <= str0.length; ++ith) {
+		for (var jth = 1; jth <= str1.length; ++jth) {
 
+			cache[ith][jth] = Math.max(
+				0,
 
+				cache[ith - 1][jth - 1] + charScore(str0.charAt(ith + 1), str1.charAt(jth + 1)),
+				cache[ith    ][jth - 1] + charScore.gap,
+				cache[ith - 1][jth    ] + charScore.gap
+			)
 
-	return align.bind(null, {
-		'charPair': charScore,
-		'gap':      -1,
-	})
+		}
+	}
 
-} )()
+	return cache[str0.length][str1.length]
+
+}
