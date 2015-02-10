db2f7f1775db6029545901a511085dfd5cfcb0c3
rgrannell1 <r.grannell2@gmail.com>
2015-02-10 05:39:23 +0000

    added setup py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-
+from setuptools import setup
 
 
 
 
 setup(
-	name        = 'engram',
-	version     = 'v0.1.0',
-	description = 'minimal bookmarking.',
-	author      = 'Ryan Grannell',
-	url         = 'https://github.com/rgrannell1/engram.py'
+	name             = 'engram',
+	version          = '0.1.0',
+	description      = 'minimal bookmarking.',
+	author           = 'Ryan Grannell',
+	url              = 'https://github.com/rgrannell1/engram.py',
+	py_modules       = ["engram"],
+	install_requires = [
+		"pystache", "requests", "httplib2", "lxml", "flask"
+	]
 )
