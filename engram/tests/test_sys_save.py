6c790cf6f04bf2bb57b8084aeebad563302faebb
rgrannell1 <r.grannell2@gmail.com>
2015-02-11 22:05:34 +0000

    added more test cases

--- engram/tests/test_sys_save.py
+++ engram/tests/test_sys_save.py
@@ -37,6 +37,8 @@ def assert_saved_correctly(uri):
 			'Connection': 'close'
 		})
 
+		open("contenttypes.txt", "a").write(response.headers['content-type'] + '\n').close()
+
 		assert response.status_code == 200
 
 		response.connection.close()
--- engram/tests/test_unit_mimetype.py
+++ engram/tests/test_unit_mimetype.py
@@ -17,6 +17,7 @@ import mimetype
 
 
 
+
 class TestMimetype(unittest.TestCase):
 	def test_tokenise_parametre(self):
 
@@ -44,6 +45,64 @@ class TestMimetype(unittest.TestCase):
 			}} ),
 			( 'multipart/x-mixed-replace; boundary="testing"', {'type': 'multipart',   'subtype': 'x-mixed-replace',         'params': {
 				'boundary': '"testing"'
+			}} ),
+
+			("text/html;",                        {'type': 'text', 'subtype': 'html',        'params': {
+
+			}} ),
+			("text/html; charset=utf-8",          {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'utf-8'
+			}} ),
+			("text/html; charset=UTF-8",          {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'UTF-8'
+			}} ),
+			("text/html;charset=utf-8",           {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'utf-8'
+			}} ),
+			("text/html; charset=ISO-8859-1",     {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'ISO-8859-1'
+			}} ),
+			("text/html;charset=UTF-8",           {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'UTF-8'
+			}} ),
+			("text/html; charset=GB2312",         {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'GB2312'
+			}} ),
+			("text/html;charset=ISO-8859-1",      {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'ISO-8859-1'
+			}} ),
+			("text/html; charset=windows-1251",   {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'windows-1251'
+			}} ),
+			("text/html; charset=Shift_JIS",      {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'Shift_JIS'
+			}} ),
+			("text/html;;charset=utf-8",          {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'utf-8'
+			}} ),
+			("text/html; charset=GBK",            {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'GBK'
+			}} ),
+			("text/html; charset=utf8",           {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'utf8'
+			}} ),
+			("text/html; charset=EUC-JP",         {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'EUC-JP'
+			}} ),
+			("text/html; charset=ISO-8859-15",    {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'ISO-8859-15'
+			}} ),
+			("text/vnd.wap.wml",                  {'type': 'text', 'subtype': 'vnd.wap.wml', 'params': {
+
+			}} ),
+			("text/html; charset=gbk",            {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'gbk'
+			}} ),
+			("text/html; charset=iso-8859-1",     {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'iso-8859-1'
+			}} ),
+			("text/html; charset=ISO-8859-2",     {'type': 'text', 'subtype': 'html',        'params': {
+				'charset': 'ISO-8859-2'
 			}} )
 		]
 
