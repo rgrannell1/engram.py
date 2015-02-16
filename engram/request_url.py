086912ca3154f7a98253e5f3b0a15b4b6a217ada
rgrannell1 <r.grannell2@gmail.com>
2015-02-16 13:04:10 +0000

    added untracked files

---
+++ engram/request_url.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import requests
+from result import Success, Failure
+
+
+
+
+
+def request_url(url):
+	"""
+	get the resource associated with a url.
+	"""
+
+	try:
+		# -- todo; eliminate pesky assignment so can be put into chain of Success then's.
+
+		user_agent = 'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 Safari/537.31'
+
+		response = requests.get(url, headers = {
+			'User-agent': user_agent,
+			'Connection': 'close'
+		}, timeout = 30)
+
+		return response
+
+	except http.client.BadStatusLine as err:
+
+		return Failure({
+			'message': "%s returned an unrecognised status." % (url, ),
+			'code':    404
+		})
+
+	except requests.exceptions.ConnectionError as err:
+
+		return Failure({
+			'message': "%s refused the connection." % (url, ),
+			'code':    404
+		})
+
+	except requests.exceptions.Timeout as err:
+
+		return Failure({
+			'message': "%s timed out." % (url, ),
+			'code':    404
+		})
+
+	except Exception as err:
+
+		return Failure(err)
