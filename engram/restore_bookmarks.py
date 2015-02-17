b20c4df751c05ec06e7edf6fb9e71da282ec8f7b
rgrannell1 <r.grannell2@gmail.com>
2015-02-17 15:59:40 +0000

    worked on uploader

---
+++ engram/restore_bookmarks.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import pages
+
+from result         import Success, Failure
+import static
+from display_result import display_result
+
+
+
+
+dependencies = {
+	'javascript-head': static.loadJS([
+
+	]),
+
+	'javascript-bottom': static.loadJS([
+		'jquery',
+		'is',
+		'import-bookmarks'
+	])
+}
+
+def restore_bookmarks(db):
+
+	page_result = (
+
+		pages.restore(dependencies)
+		.then(lambda html: {
+			'message': html,
+			'code':    200
+		})
+
+	)
+
+	return display_result(page_result)
---
+++ public/javascript/import-bookmarks.js
@@ -0,0 +1,51 @@
+
+const readDataURL = function (reader) {
+
+	const dataURL      = reader.result
+	const contentType  = "data:text/html;base64,"
+
+	if (!dataURL.startsWith(contentType)) {
+		alert('invalid data uri.')
+	} else {
+
+		const htmlBase64 = dataURL.slice(contentType.length)
+		const html       = atob(htmlBase64)
+
+	}
+
+}
+
+
+
+
+
+const uploadFile = function () {
+
+	const $uploader = $('#uploader')
+	const files     = $uploader.prop('files')
+
+	if ( is.undefined(files[0]) ) {
+		setTimeout(uploadFile, 100)
+	} else {
+
+		alert('reader')
+
+		const reader  = new FileReader()
+		reader.onload = function () {
+
+		}
+		reader.readAsDataURL(files[0])
+
+	}
+}
+
+
+
+
+
+$(function () {
+
+	$('#uploader').on('click', uploadFile)
+	$('#uploader').click()
+
+})
